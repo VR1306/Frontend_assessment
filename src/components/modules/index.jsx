b85de0ab51a30d468e856685a5b7ed11c17875e1
Vijayaraghavan1306 <mer83563@adobe.com>
2025-02-17 14:15:01 +0530

    Fourth section completed

--- src/components/modules/index.jsx
+++ src/components/modules/index.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import DeviceInformation from './device-information'
 import Specifics from './specifics/index';
 import Additional from './additional-possibilities';
+import Platforms from './platforms';
 const Home = () => {
   return (
     <React.Fragment>
       <DeviceInformation/>
       <Specifics/>
       <Additional/>
+      <Platforms/>
     </React.Fragment>
   )
 }
